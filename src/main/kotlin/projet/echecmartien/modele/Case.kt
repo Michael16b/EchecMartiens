51a02e0b664054654ec01eaaafc250b9c4c0b63f
Louis Painter <louis.painter@univ-nantes.fr>
2022-05-24 16:21:57 +0200

    Implémentation Case

--- src/main/kotlin/projet/echecmartien/modele/Case.kt
+++ src/main/kotlin/projet/echecmartien/modele/Case.kt
@@ -3,41 +3,44 @@ package projet.echecmartien.modele
 
 
 class Case {
+    private var joueur: Joueur?
+    private var pion: Pion?
+
+    init {
+        this.joueur = null
+        this.pion = null
+    }
 
     /**
      * teste si une case contient un pion ou non
      * @return true si la case ne contient pas un pion, false sinon.
      */
     fun estLibre(): Boolean {
-       TODO()
+       return pion == null
     }
 
     /** getter
      * @return le joueur associé à la case
      */
-    fun getJoueur():Joueur? {
-        TODO()
-    }
+    fun getJoueur():Joueur? = this.joueur
 
     /** setter
      * @param joueur qui est associé à la case
      */
     fun setJoueur(joueur: Joueur?) {
-        TODO()
+        this.joueur = joueur
     }
 
     /** getter
      * @return le pion associé à la case
      */
-    fun getPion():Pion? {
-        TODO()
-    }
+    fun getPion():Pion? = this.pion
 
     /** setter
      * @param pion qui est associé à la case
      */
     fun setPion(pion: Pion?) {
-       TODO()
+       this.pion = pion
     }
 
 }
