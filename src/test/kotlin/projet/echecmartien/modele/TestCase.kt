e60fc4b317cf44b711f42e46ea6f903153ef10d7
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-05-30 17:19:21 +0200

    Ajout des fichiers de test(except TestJeu)

---
+++ src/test/kotlin/projet/echecmartien/modele/TestCase.kt
@@ -0,0 +1,28 @@
+package projet.echecmartien.modele
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TestCase {
+
+    @Test
+    fun estLibre() {
+    }
+
+    @Test
+    fun getJoueur() {
+    }
+
+    @Test
+    fun setJoueur() {
+    }
+
+    @Test
+    fun getPion() {
+    }
+
+    @Test
+    fun setPion() {
+    }
+}
---
+++ src/test/kotlin/projet/echecmartien/modele/TestCoordonnee.kt
@@ -0,0 +1,20 @@
+package projet.echecmartien.modele
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TestCoordonnee {
+
+    @Test
+    fun getX() {
+    }
+
+    @Test
+    fun getY() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+}
