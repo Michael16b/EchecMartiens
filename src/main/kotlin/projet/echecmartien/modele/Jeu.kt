d7322734e00bbd96a8d67f6d9cc7b3a577640387
Matthis BLEUET <matthis.bleuet@etu.univ-nantes.fr>
2022-05-30 15:38:45 +0200

    implémentation de la classe jeu

--- src/main/kotlin/projet/echecmartien/modele/Jeu.kt
+++ src/main/kotlin/projet/echecmartien/modele/Jeu.kt
@@ -1,14 +1,27 @@
 package projet.echecmartien.modele
 
+import projet.echecmartien.librairie.TAILLEHORIZONTALE
+import projet.echecmartien.librairie.TAILLEVERTICALE
+
 
 class Jeu {
 
     private var nombreCoupsSansPrise: Int
     private var nombreCoupsSansPriseMax: Int
+    private var coordOrigine : Coordonnee
+    private var coordDest : Coordonnee
+    private var joueurCourant : Joueur?
+    private var joueurs : Array<Joueur?>
+    private var plateau : Plateau
 
     init {
         this.nombreCoupsSansPrise = 0
         this.nombreCoupsSansPriseMax = 0
+        coordOrigine = Coordonnee(0,0)
+        coordDest = Coordonnee(0,0)
+        joueurCourant = null
+        joueurs = arrayOfNulls(2)
+        plateau = Plateau()
     }
 
     /**
@@ -16,14 +29,14 @@ class Jeu {
      * @return la coordonnée origine du déplacement
      */
     fun getCoordOrigineDeplacement(): Coordonnee?{
-       TODO()
+       return coordOrigine
     }
     /**
      * getter
      * @return la coordonnée destination du déplacement
      */
     fun getCoordDestinationDeplacement(): Coordonnee?{
-        TODO()
+        return coordDest
     }
 
 
@@ -32,7 +45,7 @@ class Jeu {
      * @param origine la coordonnée origine du déplacement
      */
     fun setCoordOrigineDeplacement(origine: Coordonnee){
-       TODO()
+       coordOrigine = origine
     }
 
 
@@ -41,7 +54,7 @@ class Jeu {
      * @param destination la coordonnée destination du déplacement
      */
     fun setCoordDestinationDeplacement(destination: Coordonnee){
-        TODO()
+        coordDest = destination
     }
 
 
@@ -49,9 +62,13 @@ class Jeu {
      * @return le joueur courant
      */
     fun getJoueurCourant(): Joueur? {
-       TODO()
+       return joueurCourant
     }
 
+    fun initialiserPartie(joueur1: Joueur, joueur2: Joueur, nombreCoupsSansPriseMax : Int) {
+        initialiserJoueur(joueur1, joueur2)
+        this.nombreCoupsSansPriseMax = nombreCoupsSansPriseMax
+    }
 
     /**
      * affectation des joueurs aux cases
@@ -59,6 +76,30 @@ class Jeu {
      * @paral joueur2 second joueur
      */
     private fun initialiserJoueur(joueur1: Joueur, joueur2: Joueur) {
+        joueurs[0] = joueur1
+        joueurs[1] = joueur2
+    }
+
+    fun deplacementPossible(coordOriginX : Int, coordOriginY : Int) : Boolean {
+        if (coordOriginX !in 0 until TAILLEHORIZONTALE || coordOriginY !in 0 until TAILLEVERTICALE) {
+            return false
+        }
+        if (plateau.getCases()[coordOriginX][coordOriginY].estLibre()) {
+            return false
+        }
+
+        return true
+    }
+
+    fun deplacementPossible(coordOriginX : Int, coordOriginY : Int, coordDestinationX : Int, coordDestinationY : Int, joueur : Joueur?) : Boolean {
+        TODO()
+    }
+
+    fun deplacer(coordOriginX: Int, coordOriginY: Int, coordDestinationX: Int, coordDestinationY: Int) {
+        TODO()
+    }
+
+    fun joueurVainqueur(): Joueur? {
         TODO()
     }
 
