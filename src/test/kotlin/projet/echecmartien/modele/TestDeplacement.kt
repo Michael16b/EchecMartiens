0d19d06acbe91ded9fb83d8737c02cf5c5d16843
Matthis BLEUET <matthis.bleuet@etu.univ-nantes.fr>
2022-06-03 14:54:54 +0200

    Ajout suite tests déplacement

--- src/test/kotlin/projet/echecmartien/modele/TestDeplacement.kt
+++ src/test/kotlin/projet/echecmartien/modele/TestDeplacement.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.lang.IllegalArgumentException
 import java.util.stream.Stream
 
 internal class TestDeplacement {
@@ -69,9 +70,21 @@ internal class TestDeplacement {
         @JvmStatic
         fun horizontalProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(0, 0), Coordonnee(2, 0)), true, "le déplacement est bien horizontzal"),
-                Arguments.of(Deplacement(Coordonnee(3, 7), Coordonnee(0, 7)), true, "le déplacement est bien horizontzal"),
-                Arguments.of(Deplacement(Coordonnee(2, 2), Coordonnee(2, 5)), false, "le déplacement n'est pas horizontzal")
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 0), Coordonnee(2, 0)),
+                    true,
+                    "le déplacement est bien horizontzal"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 7), Coordonnee(0, 7)),
+                    true,
+                    "le déplacement est bien horizontzal"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(2, 2), Coordonnee(2, 5)),
+                    false,
+                    "le déplacement n'est pas horizontzal"
+                )
             )
         }
 
@@ -80,7 +93,11 @@ internal class TestDeplacement {
             return Stream.of(
                 Arguments.of(Deplacement(Coordonnee(0, 0), Coordonnee(0, 2)), true, "le déplacement est bien vertical"),
                 Arguments.of(Deplacement(Coordonnee(3, 7), Coordonnee(3, 0)), true, "le déplacement est bien vertical"),
-                Arguments.of(Deplacement(Coordonnee(2, 2), Coordonnee(3, 2)), false, "le déplacement n'est pas vertical")
+                Arguments.of(
+                    Deplacement(Coordonnee(2, 2), Coordonnee(3, 2)),
+                    false,
+                    "le déplacement n'est pas vertical"
+                )
             )
         }
 
@@ -89,61 +106,137 @@ internal class TestDeplacement {
             return Stream.of(
                 Arguments.of(Deplacement(Coordonnee(0, 0), Coordonnee(2, 2)), true, "le déplacement est bien diagonal"),
                 Arguments.of(Deplacement(Coordonnee(3, 2), Coordonnee(0, 5)), true, "le déplacement est bien diagonal"),
-                Arguments.of(Deplacement(Coordonnee(0, 2), Coordonnee(3, 2)), false, "le déplacement n'est pas diagonal")
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 2), Coordonnee(3, 2)),
+                    false,
+                    "le déplacement n'est pas diagonal"
+                )
             )
         }
 
         @JvmStatic
         fun verticalPositifProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(1, 1), Coordonnee(1, 6)), true, "le déplacement est bien vertical positif"),
-                Arguments.of(Deplacement(Coordonnee(0, 5), Coordonnee(2, 5)), false, "le déplacement n'est pas vertical positif"),
-                Arguments.of(Deplacement(Coordonnee(3, 6), Coordonnee(3, 1)), false, "le déplacement n'est pas vertical positif")
+                Arguments.of(
+                    Deplacement(Coordonnee(1, 1), Coordonnee(1, 6)),
+                    true,
+                    "le déplacement est bien vertical positif"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 5), Coordonnee(2, 5)),
+                    false,
+                    "le déplacement n'est pas vertical positif"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 6), Coordonnee(3, 1)),
+                    false,
+                    "le déplacement n'est pas vertical positif"
+                )
             )
         }
 
         @JvmStatic
         fun horizontalPositifProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(1, 1), Coordonnee(3, 1)), true, "le déplacement est bien horizontal positif"),
-                Arguments.of(Deplacement(Coordonnee(0, 5), Coordonnee(0, 1)), false, "le déplacement n'est pas horizontal positif"),
-                Arguments.of(Deplacement(Coordonnee(2, 4), Coordonnee(0, 4)), false, "le déplacement n'est pas horizontal positif")
+                Arguments.of(
+                    Deplacement(Coordonnee(1, 1), Coordonnee(3, 1)),
+                    true,
+                    "le déplacement est bien horizontal positif"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 5), Coordonnee(0, 1)),
+                    false,
+                    "le déplacement n'est pas horizontal positif"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(2, 4), Coordonnee(0, 4)),
+                    false,
+                    "le déplacement n'est pas horizontal positif"
+                )
             )
         }
 
         @JvmStatic
         fun diagonalPositifXPositifYProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(0, 1), Coordonnee(2, 3)), true, "le déplacement est bien diagonal positif en X et positif en Y"),
-                Arguments.of(Deplacement(Coordonnee(3, 1), Coordonnee(3, 6)), false, "le déplacement n'est pas diagonal positif en X et positif en Y"),
-                Arguments.of(Deplacement(Coordonnee(3, 1), Coordonnee(0, 4)), false, "le déplacement n'est pas diagonal positif en X et positif en Y")
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 1), Coordonnee(2, 3)),
+                    true,
+                    "le déplacement est bien diagonal positif en X et positif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 1), Coordonnee(3, 6)),
+                    false,
+                    "le déplacement n'est pas diagonal positif en X et positif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 1), Coordonnee(0, 4)),
+                    false,
+                    "le déplacement n'est pas diagonal positif en X et positif en Y"
+                )
             )
         }
 
         @JvmStatic
         fun diagonalPositifXNegatifYProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(0, 6), Coordonnee(2, 4)), true, "le déplacement est bien diagonal positif en X et négatif en Y"),
-                Arguments.of(Deplacement(Coordonnee(3, 1), Coordonnee(1, 1)), false, "le déplacement n'est pas diagonal positif en X et négatif en Y"),
-                Arguments.of(Deplacement(Coordonnee(3, 3), Coordonnee(1, 1)), false, "le déplacement n'est pas diagonal positif en X et négatif en Y")
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 6), Coordonnee(2, 4)),
+                    true,
+                    "le déplacement est bien diagonal positif en X et négatif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 1), Coordonnee(1, 1)),
+                    false,
+                    "le déplacement n'est pas diagonal positif en X et négatif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 3), Coordonnee(1, 1)),
+                    false,
+                    "le déplacement n'est pas diagonal positif en X et négatif en Y"
+                )
             )
         }
 
         @JvmStatic
         fun diagonalNegatifXPositifYProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(3, 1), Coordonnee(0, 4)), true, "le déplacement est bien diagonal négatif en X et positif en Y"),
-                Arguments.of(Deplacement(Coordonnee(0, 2), Coordonnee(3, 2)), false, "le déplacement n'est pas diagonal négatif en X et positif en Y"),
-                Arguments.of(Deplacement(Coordonnee(0, 3), Coordonnee(2, 1)), false, "le déplacement n'est pas diagonal négatif en X et positif en Y")
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 1), Coordonnee(0, 4)),
+                    true,
+                    "le déplacement est bien diagonal négatif en X et positif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 2), Coordonnee(3, 2)),
+                    false,
+                    "le déplacement n'est pas diagonal négatif en X et positif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 3), Coordonnee(2, 1)),
+                    false,
+                    "le déplacement n'est pas diagonal négatif en X et positif en Y"
+                )
             )
         }
 
         @JvmStatic
         fun diagonalNegatifXNegatifYProvider(): Stream<Arguments?>? {
             return Stream.of(
-                Arguments.of(Deplacement(Coordonnee(3, 6), Coordonnee(0, 3)), true, "le déplacement est bien diagonal négatif en X et négatif en Y"),
-                Arguments.of(Deplacement(Coordonnee(1, 0), Coordonnee(1, 5)), false, "le déplacement n'est pas diagonal négatif en X et négatif en Y"),
-                Arguments.of(Deplacement(Coordonnee(0, 2), Coordonnee(2, 4)), false, "le déplacement n'est pas diagonal négatif en X et négatif en Y")
+                Arguments.of(
+                    Deplacement(Coordonnee(3, 6), Coordonnee(0, 3)),
+                    true,
+                    "le déplacement est bien diagonal négatif en X et négatif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(1, 0), Coordonnee(1, 5)),
+                    false,
+                    "le déplacement n'est pas diagonal négatif en X et négatif en Y"
+                ),
+                Arguments.of(
+                    Deplacement(Coordonnee(0, 2), Coordonnee(2, 4)),
+                    false,
+                    "le déplacement n'est pas diagonal négatif en X et négatif en Y"
+                )
             )
         }
     }
