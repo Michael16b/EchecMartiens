0e53bde66c16e0609038faad0b0c52ae8952196c
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-06-03 11:52:37 +0200

    Modification initialisation de la partie

--- src/main/kotlin/projet/echecmartien/modele/Jeu.kt
+++ src/main/kotlin/projet/echecmartien/modele/Jeu.kt
@@ -61,10 +61,10 @@ class Jeu {
     fun getJoueurCourant(): Joueur? = joueurCourant
 
     fun initialiserPartie(joueur1: Joueur, joueur2: Joueur, nombreCoupsSansPriseMax : Int) {
-        initialiserJoueur(joueur1, joueur2)
         this.nombreCoupsSansPriseMax = nombreCoupsSansPriseMax
         nombreCoupsSansPrise = 0
         plateau.initialiser()
+        initialiserJoueur(joueur1, joueur2)
     }
 
     /**
@@ -76,17 +76,18 @@ class Jeu {
         joueurs[0] = joueur1
         joueurs[1] = joueur2
 
+        val cases = plateau.getCases()
         // Joueur 1, partie "haute" du plateau
         for (i in 0 until TAILLEHORIZONTALE) {
             for (j in 0 until TAILLEVERTICALE/2) {
-                plateau.getCases()[i][j].setJoueur(joueur1)
+                cases[i][j].setJoueur(joueur1)
             }
         }
 
         // Joueur 2, partie "basse" du plateau
         for (i in 0 until TAILLEHORIZONTALE) {
             for (j in TAILLEVERTICALE/2 until TAILLEVERTICALE) {
-                plateau.getCases()[i][j].setJoueur(joueur2)
+                cases[i][j].setJoueur(joueur2)
             }
         }
     }
--- src/test/kotlin/projet/echecmartien/modele/TestJeu.kt
+++ src/test/kotlin/projet/echecmartien/modele/TestJeu.kt
@@ -3,6 +3,8 @@ package projet.echecmartien.modele
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import projet.echecmartien.librairie.TAILLEHORIZONTALE
+import projet.echecmartien.librairie.TAILLEVERTICALE
 
 internal class TestJeu {
 
