8e1272b52b0c4722f0e485d38f9cb24ecf55a8b0
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-06-03 14:01:01 +0200

    Prep Ajout test equals

--- src/test/kotlin/projet/echecmartien/modele/TestCoordonnee.kt
+++ src/test/kotlin/projet/echecmartien/modele/TestCoordonnee.kt
@@ -21,6 +21,11 @@ internal class TestCoordonnee {
     fun testGetY(c:Coordonnee,oracle: Int, message: String){
         assertEquals(oracle, c.getY(), message)
     }
+    @ParameterizedTest
+    @MethodSource("coordProviderEquals")
+    fun testEquals(c1:Coordonnee,c2 :Coordonnee,oracle: Boolean, message: String){
+        assertEquals(oracle, c1.equals(c2), message)
+    }
 
     companion object {
         @JvmStatic
@@ -35,6 +40,13 @@ internal class TestCoordonnee {
             return Stream.of(
                 Arguments.of(Coordonnee(0, 2), 2, "La coordonnée en y est 2"),
                 Arguments.of(Coordonnee(5, 8), 8, "La coordonnée en y est 8")
+            )
+        }
+        @JvmStatic
+        fun coordProviderEquals(): Stream<Arguments?>? {
+            return Stream.of(
+                Arguments.of(Coordonnee(0, 2),Coordonnee(0, 2), true, "Les deux coordonnées sont égaux"),
+                Arguments.of(Coordonnee(5, 2),Coordonnee(0, 2), false, "Les deux coordonnées ne sont pas égaux")
             )
         }
     }
