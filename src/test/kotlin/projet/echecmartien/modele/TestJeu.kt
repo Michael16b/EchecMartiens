5c95bdda49a0d0dc4f613119f67c7ab8cfb025ed
Michael BESILY <Michael.besily@etu.univ-nantes.fr>
2022-06-09 10:15:54 +0200

    Merge

--- src/test/kotlin/projet/echecmartien/modele/TestJeu.kt
+++ src/test/kotlin/projet/echecmartien/modele/TestJeu.kt
@@ -1,6 +1,6 @@
 package projet.echecmartien.modele
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import projet.echecmartien.librairie.TAILLEHORIZONTALE
