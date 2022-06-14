74ec600569a6e60c1f35a0ac5c99ec4c1917207f
Julien <julien.le-juge@etu.univ-nantes.fr>
2022-06-14 16:20:58 +0200

    Ajout LoadgameVue structure 1.1 Menuprincipal delete

--- src/main/kotlin/projet/echecmartien/vue/LoadGameVue.kt
+++ src/main/kotlin/projet/echecmartien/vue/LoadGameVue.kt
@@ -1,6 +1,7 @@
 package projet.echecmartien.vue
 
 import javafx.geometry.Insets
+import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.RadioButton
@@ -16,34 +17,52 @@ class LoadGameVue(str : String = "LoadGame-s Martiens"): BorderPane()  {
     var TitrePage = Label()
     val nomPartie = TextField()
     val CasesBoutonRadio = VBox()
+    val CasesBoutons = VBox()
     val boutonChargerPartie = Button()
     val boutonSupprimerPartie = Button()
     val boutonRenommerPartie = Button()
     val boutonRetour = Button()
 
     init{
-        this.style="-fx-background-color: purple"
+        this.style="-fx-background-color: #c3b9ea "
 
         this.TitrePage = Label("Charger la partie")
         this.top = this.TitrePage
         this.left = this.nomPartie
+        //BorderPane.setAlignment(this.TitrePage,Pos.TOP_CENTER)
+        this.TitrePage.alignment = Pos.TOP_CENTER
 
         this.nomPartie.minWidth(120.0)
-        this.nomPartie.maxHeight(20.0)
+        this.nomPartie.maxHeight(30.0)
+        this.nomPartie.alignment = Pos.TOP_LEFT
 
         this.left = this.CasesBoutonRadio
+
         val ligne1 = HBox()
+        ligne1.style = "-fx-border-color: black"
         val radioButton1 = RadioButton("1")
         val titreLigne1 = Label("nom de partie1")
+        ligne1.children.addAll(radioButton1,titreLigne1)
         val ligne2 = HBox()
+        ligne2.style = "-fx-border-color: black"
         val radioButton2 = RadioButton("1")
         val titreLigne2 = Label("nom de partie2")
+        ligne2.children.addAll(radioButton2,titreLigne2)
         val ligne3 = HBox()
+        ligne3.style = "-fx-border-color: black"
         val radioButton3 = RadioButton("1")
         val titreLigne3 = Label("nom de partie3")
+        ligne3.children.addAll(radioButton3,titreLigne3)
         val ligne4 = HBox()
+        ligne4.style = "-fx-border-color: black"
         val radioButton4 = RadioButton("2")
         val titreLigne4 = Label("nom de partie4")
+        ligne4.children.addAll(radioButton4,titreLigne4)
+
+        this.CasesBoutonRadio.children.addAll(ligne1,ligne2,ligne3,ligne4)
+        this.CasesBoutonRadio.style = "-fx-border-color: black"
+
+        this.right = this.CasesBoutons
 
 
 
