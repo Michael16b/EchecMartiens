88f174967bf9a70aab535b42f15dd9db276cc69b
Michael BESILY <michael.besily@etu.univ-nantes.fr>
2022-06-14 16:20:41 +0200

    AjoutControleur LoadGameVue, GameVue

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -4,7 +4,9 @@ import javafx.application.Application
 import javafx.event.ActionEvent
 import javafx.scene.Scene
 import javafx.stage.Stage
-import projet.echecmartien.controleur.ControleurRules
+import projet.echecmartien.controleur.ControleurVueBorderPane
+import projet.echecmartien.vue.GameVue
+import projet.echecmartien.vue.LoadGameVue
 import projet.echecmartien.vue.MainVue
 import projet.echecmartien.vue.RulesVue
 
@@ -17,8 +19,19 @@ class AppliJeuEchecMartien: Application() {
         val sceneHeight = 700.0
         val scene = Scene(root, sceneWidth, sceneHeight)
         val rootRules = RulesVue()
-        val controleurRules = ControleurRules(scene,rootRules)
-        root.buttonRules.addEventHandler(ActionEvent.ACTION,controleurRules)
+        val controleurRulesVue = ControleurVueBorderPane(scene,rootRules)
+        root.buttonRules.addEventHandler(ActionEvent.ACTION,controleurRulesVue)
+
+
+        val rootGameVue = GameVue()
+        val controleurGameVue = ControleurVueBorderPane(scene,rootGameVue)
+        root.buttonRules.addEventHandler(ActionEvent.ACTION,controleurGameVue)
+
+        val rootLoadGameVue = LoadGameVue()
+        // val controleurLoadGameVue = ControleurVueBorderPane(scene,rootLoadGameVue)
+        // root.buttonRules.addEventHandler(ActionEvent.ACTION,controleurLoadGameVue)
+
+
 
         primaryStage.minHeight = sceneHeight+40.0
         primaryStage.minWidth = sceneWidth+15.0
