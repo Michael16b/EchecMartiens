e4f108f14d0ed107b8b6822e6676e1b2287ba989
LouisMDK <louis.nolan.painter@gmail.com>
2022-06-15 10:59:40 +0200

    Ajout documentation

--- src/main/kotlin/projet/echecmartien/librairie/PlayerIA.kt
+++ src/main/kotlin/projet/echecmartien/librairie/PlayerIA.kt
@@ -3,6 +3,10 @@ package projet.echecmartien.librairie
 import projet.echecmartien.modele.Deplacement
 import projet.echecmartien.modele.Jeu
 
+/**
+ * Classe qui permet par la méthode prochainCoup() de connaître le coup de l'adversaire
+ * l'ia est toujours le deuxième joueur de la partie
+ */
 class PlayerIA(modele: Jeu) {
 
     private val modele: Jeu
@@ -13,6 +17,7 @@ class PlayerIA(modele: Jeu) {
 
     /**
      * fonction qui renvoie le prochain coup que fera l'IA
+     * on suppose qu'au moins un déplacement est possible
      * @return Deplacement correspondant au déplacement du prochain coup effectué par l'ia
      */
     fun prochainCoup(): Deplacement {
