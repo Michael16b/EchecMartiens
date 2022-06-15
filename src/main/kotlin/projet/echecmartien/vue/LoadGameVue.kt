e79a45d43efb92600f374a0b0430cb374ebe543e
Julien <julien.le-juge@etu.univ-nantes.fr>
2022-06-15 15:30:22 +0200

    Ajout LoadgameVue structure 1.6 réglages

--- src/main/kotlin/projet/echecmartien/vue/LoadGameVue.kt
+++ src/main/kotlin/projet/echecmartien/vue/LoadGameVue.kt
@@ -10,7 +10,7 @@ class LoadGameVue(str : String = "LoadGame-s Martiens"): BorderPane()  {
     var TitrePageBandeau = str
 
     var TitrePage = Label()
-    val nomPartie = TextField()
+
     val CasesBoutonRadio = GridPane()
     val CasesBoutons = VBox()
     val boutonChargerPartie = Button("Charger la partie")
@@ -21,42 +21,39 @@ class LoadGameVue(str : String = "LoadGame-s Martiens"): BorderPane()  {
     init{
         this.style="-fx-background-color: #c3b9ea "
 
-
         this.TitrePage = Label("Charger la partie")
         this.TitrePage.style = "-fx-font-style : 'Cambria'"
         this.TitrePage.style = "-fx-font-size : 22"
         this.top = this.TitrePage
-        this.left = this.nomPartie
         BorderPane.setAlignment(this.TitrePage,Pos.TOP_CENTER)
+        this.TitrePage.padding = Insets(40.0,0.0,100.0,0.0)
 
-        this.nomPartie.minWidth(120.0)
-        this.nomPartie.maxHeight(30.0)
-        this.nomPartie.alignment = Pos.TOP_LEFT
 
-        //val ligne1 = HBox()
-        //ligne1.style = "-fx-border-color: black"
+
         val radioButton1 = RadioButton()
+        radioButton1.padding = Insets(40.0,10.0,40.0,10.0)
         val titreLigne1 = Label("nom de partie1")
-        //ligne1.children.addAll(radioButton1,titreLigne1)
-        //val ligne2 = HBox()
-        //ligne2.style = "-fx-border-color: black"
+        titreLigne1.padding = Insets(40.0,0.0,40.0,20.0)
+
         val radioButton2 = RadioButton()
+        radioButton2.padding = Insets(40.0,10.0,40.0,10.0)
         val titreLigne2 = Label("nom de partie2")
-        //ligne2.children.addAll(radioButton2,titreLigne2)
-        //val ligne3 = HBox()
-        //ligne3.style = "-fx-border-color: black"
+        titreLigne2.padding = Insets(40.0,0.0,40.0,20.0)
+
         val radioButton3 = RadioButton()
+        radioButton3.padding = Insets(40.0,10.0,40.0,10.0)
         val titreLigne3 = Label("nom de partie3")
-        //ligne3.children.addAll(radioButton3,titreLigne3)
-        //val ligne4 = HBox()
-        //ligne4.style = "-fx-border-color: black"
+        titreLigne3.padding = Insets(40.0,0.0,40.0,20.0)
+
         val radioButton4 = RadioButton()
+        radioButton4.padding = Insets(40.0,10.0,40.0,10.0)
         val titreLigne4 = Label("nom de partie4")
-        //ligne4.children.addAll(radioButton4,titreLigne4)
+        titreLigne4.padding = Insets(40.0,0.0,40.0,20.0)
+
 
         this.left = CasesBoutonRadio
-        this.CasesBoutonRadio.alignment = Pos.CENTER_LEFT
-        this.CasesBoutonRadio.isGridLinesVisible = false
+        this.CasesBoutonRadio.alignment = Pos.TOP_LEFT
+        this.CasesBoutonRadio.isGridLinesVisible = true
         this.CasesBoutonRadio.add(radioButton1,0,1)
         this.CasesBoutonRadio.add(titreLigne1,1,1)
         this.CasesBoutonRadio.add(radioButton2,0,2)
@@ -65,11 +62,12 @@ class LoadGameVue(str : String = "LoadGame-s Martiens"): BorderPane()  {
         this.CasesBoutonRadio.add(titreLigne3,1,3)
         this.CasesBoutonRadio.add(radioButton4,0,4)
         this.CasesBoutonRadio.add(titreLigne4,1,4)
-        this.CasesBoutonRadio.vgap = 70.0
-        this.CasesBoutonRadio.maxHeight(400.0)
-        this.CasesBoutonRadio.minWidth(300.0)
+        //this.CasesBoutonRadio.vgap = 0.0
+        this.CasesBoutonRadio.maxHeight(200.0)
+        this.CasesBoutonRadio.minWidth(100.0)
 
-        CasesBoutonRadio.padding = Insets(0.0,40.0,100.0,40.0)
+        CasesBoutonRadio.padding = Insets(0.0,40.0,0.0,40.0)
+        CasesBoutonRadio.prefWidth = 100.0
 
         val radioGroup = ToggleGroup()
         radioButton1.toggleGroup = radioGroup
@@ -80,23 +78,25 @@ class LoadGameVue(str : String = "LoadGame-s Martiens"): BorderPane()  {
         this.right = CasesBoutons
         this.CasesBoutons.alignment = Pos.CENTER_RIGHT
 
-        val column = ColumnConstraints()
-        column.percentWidth = 50.0
+        val column1 = ColumnConstraints()
+        column1.percentWidth = 5.0
+        val column2 = ColumnConstraints()
+        column2.percentWidth = 95.0
         val row = RowConstraints()
-        row.maxHeight= Double.MAX_VALUE
-        this.CasesBoutonRadio.columnConstraints.addAll(column)
+        row.prefHeight = 0.0//Double.MAX_VALUE
+        this.CasesBoutonRadio.columnConstraints.addAll(column1,column2)
         this.CasesBoutonRadio.rowConstraints.addAll(row)
         //this.CasesBoutonRadio.style = "-fx-border-color: black"
 
 
-        CasesBoutons.padding = Insets(0.0,40.0,40.0,0.0)
+        CasesBoutons.padding = Insets(0.0,40.0,0.0,0.0)
         CasesBoutons.spacing = 30.0
         boutonRetour.setPrefSize(150.0,70.0)
         boutonChargerPartie.setPrefSize(150.0,70.0)
         boutonSupprimerPartie.setPrefSize(150.0,70.0)
         boutonRenommerPartie.setPrefSize(150.0,70.0)
         this.CasesBoutons.children.addAll(boutonChargerPartie,boutonSupprimerPartie,boutonRenommerPartie,boutonRetour)
-        setMargin(CasesBoutons,Insets(20.0))
+        setMargin(CasesBoutons,Insets(0.0))
 
     }
 }
