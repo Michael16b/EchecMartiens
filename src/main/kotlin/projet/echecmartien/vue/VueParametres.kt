0676320c640d1b0383ec504a6c9e9c9c8d0a343f
matthis <matthis.bleuet@etu.univ-nantes.fr>
2022-06-16 22:57:33 +0200

    modif vue paramètres

--- src/main/kotlin/projet/echecmartien/vue/VueParametres.kt
+++ src/main/kotlin/projet/echecmartien/vue/VueParametres.kt
@@ -1,20 +1,15 @@
 package projet.echecmartien.vue
 
-
-import javafx.event.ActionEvent
 import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
-import projet.echecmartien.controleur.ControleurFullScreen
 
 
 class VueParametres : BorderPane() {
 
     val buttonContainer : VBox
 
-    val buttonMusic : Button
     val buttonFullScreen : Button
     val buttonBack : Button
 
@@ -22,20 +17,14 @@ class VueParametres : BorderPane() {
         buttonContainer = VBox()
         buttonContainer.alignment = Pos.CENTER
 
-        buttonMusic = Button("Musique")
         buttonFullScreen = Button("Plein écran")
-        //buttonFullScreen.addEventHandler(ActionEvent.ACTION, ControleurFullScreen())
         buttonBack = Button("Retour")
 
-        buttonContainer.children.addAll(buttonMusic, buttonFullScreen, buttonBack)
+        buttonContainer.children.addAll(buttonFullScreen, buttonBack)
         buttonBack.prefWidth = 200.0
         buttonFullScreen.prefWidth = 200.0
-        buttonMusic.prefWidth = 200.0
         buttonContainer.spacing = 10.0
         this.center = buttonContainer
-
-
-
     }
 
 }
