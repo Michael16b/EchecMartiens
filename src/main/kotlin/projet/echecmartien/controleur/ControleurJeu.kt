831aea19ca371e633aa67072cd29cac06f3f8431
Michaël BESILY <michael.besily@etu.univ-nantes.fr>
2022-06-16 18:02:11 +0200

    Suppression Debug ControleurJeu

--- src/main/kotlin/projet/echecmartien/controleur/ControleurJeu.kt
+++ src/main/kotlin/projet/echecmartien/controleur/ControleurJeu.kt
@@ -148,13 +148,11 @@ class ControleurJeu(scene: Scene, vue: VueJeu, modele: Jeu, joueur1: Joueur, jou
             tourSuivant()
         }
         if (modele.getJoueurCourant() == j1) {
-            println("C'est au tour du joueur 1")
             vue.labelPlayer1.styleClass.clear()
             vue.labelPlayer2.styleClass.clear()
             vue.labelPlayer1.styleClass.add("PseudoLabelSonTour")
             vue.labelPlayer2.styleClass.add("PseudoLabelPasSonTour")
         } else {
-            println("C'est au tour du joueur 2")
             vue.labelPlayer1.styleClass.clear()
             vue.labelPlayer2.styleClass.clear()
             vue.labelPlayer1.styleClass.add("PseudoLabelPasSonTour")
