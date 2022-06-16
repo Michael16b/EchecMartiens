3603bde0c7ec1b89644b50fc4475c8816d01d736
matthis <matthis.bleuet@etu.univ-nantes.fr>
2022-06-16 23:07:37 +0200

    ajout titre vue paramètre

--- src/main/kotlin/projet/echecmartien/vue/VueParametres.kt
+++ src/main/kotlin/projet/echecmartien/vue/VueParametres.kt
@@ -1,7 +1,9 @@
 package projet.echecmartien.vue
 
+import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.VBox
 
@@ -9,13 +11,25 @@ import javafx.scene.layout.VBox
 class VueParametres : BorderPane() {
 
     val buttonContainer : VBox
-
+    val labelTitreContainer : VBox
+    val labelTitre : Label
     val buttonFullScreen : Button
     val buttonBack : Button
 
     init {
         buttonContainer = VBox()
         buttonContainer.alignment = Pos.CENTER
+
+        labelTitre = Label("Paramètres")
+        labelTitre.style = "-fx-font-family : 'Cambria'; -fx-font-size : 50; -fx-font-weight : bold"
+        labelTitre.alignment = Pos.CENTER
+        labelTitre.padding = Insets(40.0,0.0,0.0,0.0)
+
+        labelTitreContainer = VBox()
+        labelTitreContainer.children.addAll(labelTitre)
+        labelTitreContainer.alignment = Pos.CENTER
+
+        this.top = labelTitreContainer
 
         buttonFullScreen = Button("Plein écran")
         buttonBack = Button("Retour")
