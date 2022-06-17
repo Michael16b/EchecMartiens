2c739ea9ee29547c413cdbcfe48e7283bd11165d
E211731S <louis.painter@univ-nantes.fr>
2022-06-17 09:31:28 +0200

    Changement image jpg en png

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -4,7 +4,6 @@ import javafx.application.Application
 import javafx.event.ActionEvent
 import javafx.scene.Scene
 import javafx.scene.image.Image
-import javafx.scene.image.ImageView
 import javafx.scene.input.KeyCombination
 import javafx.stage.Stage
 import javafx.stage.WindowEvent
